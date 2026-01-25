ee86599cd763670ba00222d3fd3d0eaf4f908175
ShimiManashirov <122288727+ShimiManashirov@users.noreply.github.com>
2026-01-25 14:41:02 +0200

    Delete dist directory

--- dist/controllers/auth_controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from 'express';
-declare const _default: {
-    register: (req: Request, res: Response) => Promise<Response<any, Record<string, any>> | undefined>;
-    login: (req: Request, res: Response) => Promise<Response<any, Record<string, any>> | undefined>;
-    logout: (req: Request, res: Response) => Promise<Response<any, Record<string, any>> | undefined>;
-    refresh: (req: Request, res: Response) => Promise<Response<any, Record<string, any>> | undefined>;
-};
-export default _default;
