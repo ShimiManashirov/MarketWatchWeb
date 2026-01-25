669cd5031e197c8829011f09ec84368497b24852
ShimiManashirov <122288727+ShimiManashirov@users.noreply.github.com>
2026-01-25 14:42:09 +0200

    Delete dist directory (#6)

--- dist/middleware/auth_middleware.d.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-export interface AuthRequest extends Request {
-    user?: {
-        _id: string;
-    };
-    file?: Express.Multer.File;
-}
-export declare const authMiddleware: (req: AuthRequest, res: Response, next: NextFunction) => Response<any, Record<string, any>> | undefined;
