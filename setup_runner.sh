5c2c975fceb04bb09237358840e5a3f9c9befbff
Tamir26 <98471911+Tamir26@users.noreply.github.com>
2026-03-14 20:48:47 +0200

    Update runner version to 2.332.0 in setup script

--- setup_runner.sh
+++ setup_runner.sh
@@ -19,10 +19,10 @@ echo "------------------------------------"
 mkdir -p actions-runner && cd actions-runner
 
 # 2. Download the latest runner package
-curl -o actions-runner-linux-x64-2.314.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.314.1/actions-runner-linux-x64-2.314.1.tar.gz
+curl -o actions-runner-linux-x64-2.332.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.332.0/actions-runner-linux-x64-2.332.0.tar.gz
 
 # 3. Extract the installer
-tar xzf ./actions-runner-linux-x64-2.314.1.tar.gz
+tar xzf ./actions-runner-linux-x64-2.332.0.tar.gz
 
 # 4. Configure the runner (User will need to provide the token from GitHub UI)
 echo "--------------------------------------------------------"
